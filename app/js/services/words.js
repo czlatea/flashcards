c79776a0f9ec5fc6e4e60ec6f3332013b6706410
Cristian Florin Zlatea <cristian.florin@zlatea.com>
2014-03-30 22:08:11 +0200

    Additional words

--- app/js/services/words.js
+++ app/js/services/words.js
@@ -4,5 +4,7 @@
   { id: 2, level: 1, de: 'Gutten Morgen', en: 'Good morning' },
   { id: 3, level: 1, de: 'Gutten Tag', en: 'Good day' },
   { id: 4, level: 1, de: 'Gutten Abend', en: 'Good evening' },
-  { id: 5, level: 1, de: 'Gutte Nacht', en: 'Good night' }
+  { id: 5, level: 1, de: 'Gutte Nacht', en: 'Good night' },
+  { id: 6, level: 1, de: 'Willkommen', en: 'Welcome' },
+  { id: 7, level: 1, de: 'Auf Widersehen', en: 'Goody bye' }
 ]
